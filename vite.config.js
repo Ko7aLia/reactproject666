8f40dd743d53d9c3d7f27bc549d6e5b6abbd5c85
Ruby Undying <Ko7aLia@yandex.ru>
2025-08-03 13:15:46 +0400

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/reactproject666",
     plugins: [plugin(),tailwindcss()],
     server: {
         port: 53321,
