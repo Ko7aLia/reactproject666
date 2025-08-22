b166261f8f95c6626ebb6fb9c10278ddf42fb543
Ruby Undying <Ko7aLia@yandex.ru>
2025-08-22 15:58:27 +0400

    fix_font

--- src/AppLogo.jsx
+++ src/AppLogo.jsx
@@ -14,7 +14,7 @@ function MainLogo() {
                       <img src={mainLogo} className="mt-[20px] mb-[40px] md:mb-[108px]" alt="Main logo" />
               </DragSvgLock>
                       <p className="m-0 p-0 font-normal">Ваш баланс:</p>
-                      <p className="m-0 p-0 leading-[100px] font-semibold text-[72px] md:text-[128px]">75 904.06</p>
+                      <p className="m-0 p-0 whitespace-nowrap leading-[100px] font-semibold text-[72px] md:text-[128px]">75 904.06</p>
               <DragSvgLock>
                       <p className="mt-[8px] mb-[56px] flex gap-1 font-medium"> <img src={star} alt="star" />Бонусов</p>
               </DragSvgLock>
